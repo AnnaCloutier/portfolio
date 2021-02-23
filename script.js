90f3a548cf8d6cfcaaf4a7d9fde909fe352deaf5
Anna Cloutier <79015563+AnnaCloutier@users.noreply.github.com>
2021-02-22 22:05:41 -0500

    First upload of files

---
+++ script.js
@@ -0,0 +1,12 @@
+// there is no JavasScript for the portfolio yet
+
+
+
+	let countDown = [];
+	for (let i = 10; i >= 1; i--) {
+		countDown.push(i)
+	}
+	
+console.log(countDown + ", Blast off!")
+
+
